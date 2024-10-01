deafd40e2aca295ed7788449252b3694bbde4dfe
José Miguel Isidro <93340308+jmisidro@users.noreply.github.com>
2024-10-01 14:53:21 +0100

    added draw_entities_season function to retrieve graphs of entities per season

--- data/src/ner.py
+++ data/src/ner.py
@@ -1,6 +1,7 @@
 import spacy
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 # Load spaCy's English model
 nlp = spacy.load("en_core_web_sm")
@@ -21,16 +22,28 @@ def extract_entities(text):
     entities = [(ent.text, ent.label_) for ent in doc.ents]
     return entities
 
-def output_entities_episode(df : pd.DataFrame, output_path : Path):
-    Path(output_path).touch(exist_ok=True)
-    with open(output_path, "w") as f:
-        for index, row in df.iterrows():
-            row['transcript'] = str(row['transcript']) # Ensure 'transcript' is a string
-            entities = extract_entities(row['transcript'])
-            f.write(f"Episode {index + 1}:\n")
-            for entity in entities:
-                f.write(f"{entity[0]}: {entity[1]}\n")
-            f.write("\n")
-        f.close()
+# Get entities from each episode
+def get_entities_episode(df : pd.DataFrame):
+    entities_list = []
+    for _, row in df.iterrows():
+        entities = extract_entities(str(row['transcript']))
+        entities_list.append(entities)
+    return entities_list
 
-output_entities_episode(df, f"{documents_output_dir_path}/entities_episode.txt")
+# Draw entities plot, grouping episodes by 'season'
+def draw_entities_season(df : pd.DataFrame):
+    for season in df['season'].unique():
+        entities_list = get_entities_episode(df[df['season'] == season])
+        entities_df = pd.DataFrame([entity for entities in entities_list for entity in entities], columns=['Entity', 'Label'])
+        if not entities_df.empty:
+            entities_df['Frequency'] = entities_df.groupby('Entity')['Entity'].transform('count')
+            entities_df.drop_duplicates(subset=['Label'], inplace=True)
+            entities_df.plot(kind='bar', x='Entity', y='Frequency')
+            plt.xlabel('Entities')
+            plt.ylabel('Frequency')
+            plt.title(f"Entities in Season {season}")
+            plt.savefig(f"{documents_output_dir_path}/entities_season_{season}.png")
+
+            plt.close()
+
+draw_entities_season(df)
